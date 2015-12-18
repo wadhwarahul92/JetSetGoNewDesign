ad8c2d6414900a33c630a3d2005d2e8cb3327437
Suraj Pratap <surajpratap@icloud.com>
2015-12-18 15:32:29 +0530

    added cities

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -3,7 +3,30 @@ class Admin::CitiesController < Admin::BaseController
   before_filter :authenticate_admin
 
   def index
+    @cities = City.all
+  end
 
+  def new
+    @city = City.new
+  end
+
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      flash[:success] = 'City created successfully'
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(
+        :name,
+        :state
+    )
   end
 
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,6 @@
 class City < ActiveRecord::Base
   #####VALIDATIONS###
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :state }
   validates :state, presence: true
   ###################
 end
