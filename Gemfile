eb95e90410864024945b1878969051ea1e6367bb
Suraj Pratap <surajpratap@icloud.com>
2015-12-26 18:34:11 +0530

    added list page

--- Gemfile
+++ Gemfile
@@ -61,6 +61,6 @@ gem 'paperclip', '~> 4.3'
 
 gem 'aws-sdk'
 
-gem 'rails_12factor'
+gem 'rails_12factor', group: :production
 
 gem 'paper_trail', '~> 4.0.0'
--- app/assets/javascripts/jetsteals.js
+++ app/assets/javascripts/jetsteals.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require ./vendors/angular
 //= require_tree ./common
 //= require_tree ./jetsteals
