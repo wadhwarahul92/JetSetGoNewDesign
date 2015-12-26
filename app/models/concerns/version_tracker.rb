572e9341d98493e1d862fdb10e8dbcabdcec163c
Suraj Pratap <surajpratap@icloud.com>
2015-12-26 14:47:36 +0530

    added last updater in cities

--- app/models/concerns/version_tracker.rb
+++ app/models/concerns/version_tracker.rb
@@ -11,4 +11,15 @@ module VersionTracker
     end
   end
 
+  def last_updater
+    last_version = self.versions.last
+    user_id = nil
+    user_id = last_version.whodunnit if last_version
+    if user_id
+      User.where(id: user_id).first
+    else
+      nil
+    end
+  end
+
 end
