840099795d16e49c7a16cbc484abf2d4ff8dde8f
Suraj Pratap <surajpratap@icloud.com>
2015-12-26 14:22:52 +0530

    further improvements in s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
           :secret_access_key => ENV['S3_BUCKET_SECRET']
       },
       :url =>':s3_domain_url',
-      :path => '/:class/:attachment/:id_partition/:style/:filename'
+      :s3_protocol => 'https',
+      :s3_host_name => 's3-ap-southeast-1.amazonaws.com'
   }
 end
