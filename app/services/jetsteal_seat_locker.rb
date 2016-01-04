eb28d0d52dff24ec4a7dfdc3607d908996c9436d
Suraj Pratap <surajpratap@icloud.com>
2016-01-04 11:44:46 +0530

    seat locking

---
+++ app/services/jetsteal_seat_locker.rb
@@ -0,0 +1,8 @@
+class JetstealSeatLocker
+
+  #when a user is about to pay for some chosen seats
+  #those seats are locked for some time to ensure that someone else dont pay for same seats
+
+  MAX_LOCK_TIME = '5 min'
+
+end
