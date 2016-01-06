6567df56d5075c39172605fad678f9b2735b11c4
Mayur <mayur.singh@jetsetgo.in>
2016-01-06 18:36:21 +0530

    filters by conditions

--- app/helpers/aircraft_types_helper.rb
+++ app/helpers/aircraft_types_helper.rb
@@ -4,4 +4,8 @@ module AircraftTypesHelper
     AircraftType.all.collect{ |c| [c.name, c.id] }
   end
 
+  def current_aircraft_types_for_collection(jetsteals)
+    jetsteals.collect{ |c| [c.aircraft.aircraft_type.name, c.aircraft.aircraft_type.id] }.uniq
+  end
+
 end
--- app/services/jetsteal_list_creator.rb
+++ app/services/jetsteal_list_creator.rb
@@ -10,7 +10,32 @@ class JetstealListCreator
     @list = Jetsteal.ready_for_sale.includes(:departure_airport, :arrival_airport, :aircraft, :jetsteal_seats)
     @list = @list.where(departure_airport_id: @params[:departure_airport_id]) if @params[:departure_airport_id].present?
     @list = @list.where(arrival_airport_id: @params[:arrival_airport_id]) if @params[:arrival_airport_id].present?
+    @list = filter_by_aircraft_id(@list) if @params[:aircraft_type_id].present?
+    @list = filter_by_wifi(@list) if @params[:wifi].present? && @params[:wifi] == 'Yes'
+    @list = filter_by_phone(@list) if @params[:phone].present? && @params[:phone] == 'Yes'
+    @list = filter_by_condition(@list, @params[:crew], 'crew') if @params[:crew].present?
+    @list = filter_by_condition(@list, @params[:flight_attendant], 'flight_attendant') if @params[:flight_attendant].present?
     @list
+  end
+
+  def filter_by_aircraft_id(list)
+    list.joins(:aircraft).merge(Aircraft.where(:aircraft_type_id => @params[:aircraft_type_id]))
+  end
+
+  def filter_by_wifi(list)
+    list.joins(:aircraft).merge(Aircraft.where(:wifi => true))
+  end
+
+  def filter_by_phone(list)
+    list.joins(:aircraft).merge(Aircraft.where(phone: true))
+  end
+
+  def filter_by_condition(list,condition1,condition2)
+    if condition1 == 'Yes'
+      list.joins(:aircraft).merge(Aircraft.where("#{condition2} > 0"))
+    else
+      list.joins(:aircraft).merge(Aircraft.where("#{condition2} < 1"))
+    end
   end
 
 end
