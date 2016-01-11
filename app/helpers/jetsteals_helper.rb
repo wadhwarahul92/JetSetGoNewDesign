4f131db3355f5cbd334a6272af8c26375df70542
Suraj Pratap <surajpratap@icloud.com>
2016-01-11 18:22:01 +0530

    worked on ui at list

--- app/helpers/jetsteals_helper.rb
+++ app/helpers/jetsteals_helper.rb
@@ -34,10 +34,10 @@ module JetstealsHelper
 
 
     view = ' '
-    view += "<li data-toggle='tooltip' title='Seating capacity'><i class='fa fa-user'> #{seat}</i></li>" if seat.present?
+    view += "<li data-toggle='tooltip' title='Seating capacity'><i class='fa fa-user'></i> #{seat}</li>" if seat.present?
     view += "<li data-toggle='tooltip' title='Wifi'><i class='fa fa-wifi'></i></li>" if wifi && !params[:wifi].present?
     view += "<li data-toggle='tooltip' title='phone'><i class='fa fa-phone'></i></li>" if phone && !params[:phone].present?
-    view += "<li data-toggle='tooltip' title='Flight Attendant'><i class='fa fa-female '></i> #{attendant}</li> " if attendant.present? && !(attendant < 1) && !params[:flight_attendant].present?
+    view += "<li data-toggle='tooltip' title='Flight Attendant'><i class='fa fa-female '></i></li> " if attendant.present? && !(attendant < 1) && !params[:flight_attendant].present?
 
     view.html_safe
   end
