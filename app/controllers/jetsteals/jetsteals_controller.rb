7faca5597da80e71f0a698bea0fdbfefa65b3aa9
Mayur <mayur.singh@jetsetgo.in>
2016-01-12 15:23:21 +0530

    update

--- app/controllers/jetsteals/jetsteals_controller.rb
+++ app/controllers/jetsteals/jetsteals_controller.rb
@@ -3,14 +3,13 @@ class Jetsteals::JetstealsController < Jetsteals::BaseController
   before_action :set_jetsteal
 
   def buy_as_whole
-
+    @whole_jet = params[:buy_as_whole] if params[:buy_as_whole].present?
   end
 
   private
 
   def set_jetsteal
     @jetsteal = Jetsteal.find params[:id]
-    @whole_jet = params[:buy_as_whole] if params[:buy_as_whole].present?
   end
 
 end
