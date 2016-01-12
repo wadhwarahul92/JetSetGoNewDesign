faae6ce1a46c113ed9a1f903f82db9e6b34b18df
Mayur <mayur.singh@jetsetgo.in>
2016-01-12 15:21:24 +0530

    whole plane error fixed in new Tab

--- app/controllers/jetsteals/jetsteals_controller.rb
+++ app/controllers/jetsteals/jetsteals_controller.rb
@@ -10,6 +10,7 @@ class Jetsteals::JetstealsController < Jetsteals::BaseController
 
   def set_jetsteal
     @jetsteal = Jetsteal.find params[:id]
+    @whole_jet = params[:buy_as_whole] if params[:buy_as_whole].present?
   end
 
 end
