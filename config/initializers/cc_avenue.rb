99d61b5beb933043e49d56a275e8344c248f7ce2
Suraj Pratap <surajpratap@icloud.com>
2016-01-14 15:28:22 +0530

    cc avenue

--- config/initializers/cc_avenue.rb
+++ config/initializers/cc_avenue.rb
@@ -2,9 +2,9 @@ CC_MERCHANT_ID = ENV['CC_MERCHANT_ID']
 CC_ACCESS_KEY = ENV['CC_ACCESS_KEY']
 CC_WORKING_KEY = ENV['CC_WORKING_KEY']
 if Rails.env.production?
-  CC_URL = 'https://test.ccavenue.com'
-else
   CC_URL = 'https://secure.ccavenue.com'
+else
+  CC_URL = 'https://test.ccavenue.com'
 end
 #raise error if cc avenue is not present
 unless CC_MERCHANT_ID.present? or CC_ACCESS_KEY.present? or CC_WORKING_KEY.present?
