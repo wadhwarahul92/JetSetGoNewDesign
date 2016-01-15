fbfeb34ef56d0419175090648fe904ec930d778a
Suraj Pratap <surajpratap@icloud.com>
2016-01-15 18:14:02 +0530

    error pages

--- app/services/jetsteal_launcher.rb
+++ app/services/jetsteal_launcher.rb
@@ -6,6 +6,7 @@ class JetstealLauncher
   end
 
   def launch!
+    return false unless check_airport_distance
     if @jetsteal.sell_by_seats?
       validate_per_seat and check_aircraft_images and @jetsteal.update_attribute :launched, true
     else
@@ -52,6 +53,16 @@ class JetstealLauncher
       end
     end
     true
+  end
+
+  def check_airport_distance
+    d = Distance.where(from_airport_id: @jetsteal.departure_airport_id, to_airport_id: @jetsteal.arrival_airport_id).first
+    if d.present?
+      true
+    else
+      @message = 'Distance from Departure to Arrival airport is not present. Go to distance and create an entry for same.'
+      false
+    end
   end
 
 end
