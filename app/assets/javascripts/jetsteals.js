43fa1301b12ed6a09393ed3e9a524aff226f0850
Suraj Pratap <surajpratap@icloud.com>
2016-01-21 12:41:42 +0530

    validations

--- app/assets/javascripts/jetsteals.js
+++ app/assets/javascripts/jetsteals.js
@@ -7,13 +7,16 @@
 //= require ./vendors/foundation-datepicker
 //= require ./vendors/underscore
 //= require ./vendors/loading-bar
-//= require vendors/select2.js
+//= require ./vendors/select2.js
+//= require ./vendors/jquery_validate
 
 $(document).on('ready page:load', function(){
+
     $(document).foundation();
 
     if(window.localStorage['visited_jetsteals']){
-
+        //do nothing
+        //user has visited jetsteal before
     }else{
         $('#info_button')[0].click();
         window.localStorage['visited_jetsteals'] = true
@@ -29,5 +32,21 @@ $(document).on('ready page:load', function(){
     }
 
     $('.airports').select2({dropdownAutoWidth : true});
+
+    $('#new_jetsteal_subscription').validate({
+        jetsteal_subscription_name: {
+            required: true
+        },
+        jetsteal_subscription_phone: {
+            required: true,
+            digits: true,
+            minlength: 10,
+            maxlength: 10
+        },
+        jetsteal_subscription_email: {
+            required: true,
+            email: true
+        }
+    })
 
 });
