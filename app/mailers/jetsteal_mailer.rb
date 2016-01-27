ac32c9c4226b4583d0daec93dbd5154615bdd192
Suraj Pratap <surajpratap@icloud.com>
2016-01-27 15:01:21 +0530

    added invoice in email

--- app/mailers/jetsteal_mailer.rb
+++ app/mailers/jetsteal_mailer.rb
@@ -11,6 +11,38 @@ class JetstealMailer < ApplicationMailer
     @transaction = transaction
     @contact = contact
 
+    ###creating invoice on invoice system
+    url = nil
+    if Rails.env.development?
+      url = 'http://localhost:3001/api/v1'
+    else
+      url = 'http://jet-set-go-finance.herokuapp.com/api/v1'
+    end
+    #creating pro_forma_invoice
+    response = Net::HTTP.post_form(URI("#{url}/create_pro_forma"), {
+      client_name: @contact.full_name,
+      aircraft: @jetsteal.aircraft.aircraft_type.name,
+      departure_airport: @jetsteal.departure_airport.name,
+      arrival_airport: @jetsteal.arrival_airport.name,
+      amount: @transaction.amount,
+      token: 'HHJynwMowx9iyBUfMk2uJw'
+    })
+    if response.code == '200'
+      pro_forma_id = JSON.parse(response.body)['id']
+      response = Net::HTTP.post_form(URI("#{url}/create_invoice"), {
+          pro_forma_invoice_id: pro_forma_id,
+          token: 'HHJynwMowx9iyBUfMk2uJw'
+      })
+      if response.code == '200'
+        invoice_id = JSON.parse(response.body)['id']
+        response = Net::HTTP.get(URI("#{url}/invoice/#{invoice_id}?token=HHJynwMowx9iyBUfMk2uJw&format=pdf"))
+        if response[0..3] == "%PDF"
+          attachments['invoice.pdf'] = response
+        end
+      end
+    end
+    ###########################
+
     attachments['Jetsteal seat confirmation.pdf'] = WickedPdf.new.pdf_from_string(
                                                                      render_to_string('pdfs/jetsteal_confirmation', layout: 'pdf')
     )
