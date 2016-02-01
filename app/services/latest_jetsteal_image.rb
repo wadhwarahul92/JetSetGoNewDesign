2a54d33c4a4f29901c9a004ca7807f8fe8a6d66f
Suraj Pratap <surajpratap@icloud.com>
2016-02-01 17:38:55 +0530

    more on footer image

--- app/services/latest_jetsteal_image.rb
+++ app/services/latest_jetsteal_image.rb
@@ -1,18 +1,22 @@
 class LatestJetstealImage
 
+  #todo optimise this to search for image of that jetsteal first and return it, if present. This will save lots of processing
+
   require 'RMagick'
 
   def initialize
     @jetsteal = Jetsteal.ready_for_sale.last
     @canvas = Magick::ImageList.new
-    @canvas.new_image(400, 100, Magick::SolidFill.new('rgb(58, 181, 127)'))
+    @canvas.new_image(400, 110, Magick::SolidFill.new('rgb(58, 181, 127)'))
+    top_image = Magick::Image.read('http://d3tfanr7troppj.cloudfront.net/static_files/images/000/000/038/original/private-jet-img.png?1454328396').first
+    @canvas.composite!(top_image, 0, 0, Magick::OverCompositeOp)
   end
 
   def draw
 
     text = Magick::Draw.new
     text.font_family = 'sans-serif'
-    text.annotate(@canvas, 100, 50, 10+55, 10, 'Book a private jet, from') do
+    text.annotate(@canvas, 10, 10, 10, 50, 'From:') do
       self.fill = 'white'
       self.font_weight = Magick::BoldWeight
       self.gravity = Magick::WestGravity
@@ -21,11 +25,11 @@ class LatestJetstealImage
 
     departure_city_image = Magick::Image.read(@jetsteal.departure_airport.city.image).first
     departure_city_image = departure_city_image.resize_to_fill(30)
-    @canvas.composite!(departure_city_image, 180+55, 7, Magick::OverCompositeOp)
+    @canvas.composite!(departure_city_image, 60, 35, Magick::OverCompositeOp)
 
     text_3 = Magick::Draw.new
     text_3.font_family = 'sans-serif'
-    text_3.annotate(@canvas, 50, 12, 174+55, 40, @jetsteal.departure_airport.city.name) do
+    text_3.annotate(@canvas, 50, 12, 50, 70, @jetsteal.departure_airport.city.name) do
       self.fill = 'white'
       self.gravity = Magick::CenterGravity
       self.font_weight = Magick::BoldWeight
@@ -34,46 +38,47 @@ class LatestJetstealImage
 
     text_2 = Magick::Draw.new
     text_2.font_family = 'sans-serif'
-    text_2.annotate(@canvas, 15, 50, 225+55, 10, 'to') do
+    text_2.annotate(@canvas, 10, 10, 100, 50, 'To:') do
       self.fill = 'white'
       self.font_weight = Magick::BoldWeight
-      self.gravity = Magick::CenterGravity
+      self.gravity = Magick::WestGravity
+      self.pointsize = 15
+    end
+
+    text_2 = Magick::Draw.new
+    text_2.font_family = 'sans-serif'
+    text_2.annotate(@canvas, 10, 10, 180, 50, @jetsteal.end_at.strftime('On: %d %b %Y At: %I:%M %p')) do
+      self.fill = 'white'
+      self.font_weight = Magick::BoldWeight
+      self.gravity = Magick::WestGravity
       self.pointsize = 15
     end
 
     arrival_city_image = Magick::Image.read(@jetsteal.arrival_airport.city.image).first
     arrival_city_image = arrival_city_image.resize_to_fit(30)
-    @canvas.composite!(arrival_city_image, 247+55, 7, Magick::OverCompositeOp)
+    @canvas.composite!(arrival_city_image, 130, 35, Magick::OverCompositeOp)
 
     text_4 = Magick::Draw.new
     text_4.font_family = 'sans-serif'
-    text_4.annotate(@canvas, 50, 12, 241+55, 40, @jetsteal.arrival_airport.city.name) do
+    text_4.annotate(@canvas, 50, 12, 130, 70, @jetsteal.arrival_airport.city.name) do
       self.fill = 'white'
-      self.gravity = Magick::CenterGravity
+      self.gravity = Magick::WestGravity
       self.font_weight = Magick::BoldWeight
       self.pointsize = 8
     end
 
-    text_5 = Magick::Draw.new
-    text_5.font_family = 'sans-serif'
-    text_5.annotate(@canvas, 0, 0, 0, 15, @jetsteal.end_at.strftime('on %d %b %Y at %I:%M %p')) do
-      self.fill = 'white'
-      self.font_weight = Magick::BoldWeight
-      self.gravity = Magick::CenterGravity
-    end
-
     if @jetsteal.sell_by_seats
-      cost_text = "starting @ INR #{@jetsteal.min_seat_cost}"
+      cost_text = "#{@jetsteal.min_seat_cost}"
     else
-      cost_text = "starting @ INR #{@jetsteal.cost}"
+      cost_text = "#{@jetsteal.cost}"
     end
 
-    text_5 = Magick::Draw.new
-    text_5.font_family = 'sans-serif'
-    text_5.annotate(@canvas, 0, 0, 0, 35, cost_text) do
-      self.fill = 'white'
+    text_4 = Magick::Draw.new
+    text_4.font_family = 'sans-serif'
+    text_4.annotate(@canvas, 50, 12, 224, 93, cost_text) do
+      self.fill = 'rgb(13, 60, 124)'
+      self.gravity = Magick::WestGravity
       self.font_weight = Magick::BoldWeight
-      self.gravity = Magick::CenterGravity
       self.pointsize = 15
     end
 
