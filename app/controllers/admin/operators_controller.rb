f340eb3d04dfd3411277f59eb19ace54adf17a43
Mayur <mayur.singh@jetsetgo.in>
2016-02-04 13:45:51 +0530

    operator in  admin

---
+++ app/controllers/admin/operators_controller.rb
@@ -0,0 +1,26 @@
+class Admin::OperatorsController < Admin::BaseController
+	before_filter :authenticate_admin
+
+	before_filter :set_operator, only: [:edit, :update]
+
+	def index
+		@operators = Operator.all
+	end
+
+	def edit
+	
+	end
+
+	def update
+		if @operator.update_attribute(:approved_by_admin, params[:approved_by_admin])
+      redirect_to action: :index
+    end
+	end
+
+	private
+
+	def set_operator
+    @operator = Operator.find params[:id]
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
 
     resources :airports
 
+    resources :operators
+
     resources :aircraft_types
 
     resources :aircrafts do
