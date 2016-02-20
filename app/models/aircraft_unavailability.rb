49219aac82b5d85e58f19513a1477882afda5442
Suraj Pratap <surajpratap@icloud.com>
2016-02-20 15:10:25 +0530

    bug fixe

--- app/models/aircraft_unavailability.rb
+++ app/models/aircraft_unavailability.rb
@@ -16,10 +16,21 @@ class AircraftUnavailability < ActiveRecord::Base
 
   validate :duplicate_times, on: :create
 
+  validate :duplicate_times_update, on: :update
+
   def duplicate_times
     if self.aircraft_id.present? and self.start_at.present? and self.end_at.present?
       if self.class.where(aircraft_id: self.aircraft_id).where('? BETWEEN start_at AND end_at', self.start_at).any? or self.class.where(aircraft_id: self.aircraft_id).where('? BETWEEN start_at AND end_at', self.end_at).any?
         self.errors.add(:base, "There's already an unavailability for this aircraft in given time")
+      end
+    end
+  end
+
+  def duplicate_times_update
+    if self.id.present?
+      unavailabilities = self.class.where(aircraft_id: self.id).where('id != ?', self.id)
+      if unavailabilities.where('? BETWEEN start_at AND end_at', self.start_at).any? or unavailabilities.where('? BETWEEN start_at AND end_at', self.end_at).any?
+        self.errors.add :base, "There's already an unavailability for this aircraft in given time"
       end
     end
   end
