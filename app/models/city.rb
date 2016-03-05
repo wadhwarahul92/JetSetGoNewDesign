1f82f306e4818a2e4ec3b22c3a43f2baad187a36
MAYUR SINGH <mayur.singh@jetsetgo.in>
2016-03-05 11:13:48 +0530

    conflict

--- app/models/city.rb
+++ app/models/city.rb
@@ -9,10 +9,26 @@ class City < ActiveRecord::Base
   has_attached_file :image, styles: {small: '50x50!'}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  ######################################################################
+  # Description: Accomodation cost tier wise for cities (this remains constant)
+  ######################################################################
+  TIER_ACCOMMODATION_COST = {
+      tier1: 50000,
+      tier2: 30000,
+      tier3: 20000
+  }
+  ######################################################################
+
+  ######################################################################
+  # Description: There is a typo mistake in "accomodation_category"
+  ######################################################################
+  # dont know why this produces error :(
+  # alias_method :accommodation_category, :accomodation_category
+
   #####VALIDATIONS###
   validates :name, presence: true, uniqueness: { scope: :state }
   validates :state, presence: true
   validates :image, presence: true
-  validates :accomodation_category, presence: true
+  validates :accomodation_category, presence: true, inclusion: { in: %w{tier1 tier2 tier3} }
   ###################
 end
--- app/services/search_algorithm.rb
+++ app/services/search_algorithm.rb
@@ -8,6 +8,7 @@ class SearchAlgorithm
   def results
     find_aircrafts
     make_results
+    make_intermediate_activities
     @results
   end
 
@@ -29,7 +30,7 @@ class SearchAlgorithm
 
       @results << {
           aircraft_id: aircraft.id,
-          flight_plan: flight_plan(aircraft)
+          flight_plan: flight_plan(aircraft),
       }
 
     end
@@ -48,7 +49,7 @@ class SearchAlgorithm
       plan << {
           departure_airport_id: aircraft.base_airport.id,
           arrival_airport_id: search_activities.first.departure_airport.id,
-          flight_type: 'ferry',
+          flight_type: 'empty_leg',
           start_at: search_activities.first.start_at - 45.minutes - aircraft.flight_time_in_hours_for(aircraft.base_airport, search_activities.first.departure_airport).hours ,
           end_at: search_activities.first.start_at - 45.minutes,
           landing_cost_at_arrival: search_activities.first.departure_airport.landing_cost,
@@ -60,6 +61,7 @@ class SearchAlgorithm
     # add plans for all user search activities
     ##########################################
     search_activities.each do |search_activity|
+      # noinspection RubyResolve
       plan << {
           departure_airport_id: search_activity.departure_airport.id,
           arrival_airport_id: search_activity.arrival_airport.id,
@@ -78,7 +80,7 @@ class SearchAlgorithm
       plan << {
           departure_airport_id: search_activities.last.arrival_airport.id,
           arrival_airport_id: aircraft.base_airport.id,
-          flight_type: 'ferry',
+          flight_type: 'empty_leg',
           start_at: plan.last[:end_at] + 45.minutes,
           end_at: plan.last[:end_at] + 45.minutes + aircraft.flight_time_in_hours_for(search_activities.last.arrival_airport, aircraft.base_airport).hours,
           landing_cost_at_arrival: aircraft.base_airport.landing_cost,
@@ -97,6 +99,83 @@ class SearchAlgorithm
     # return plan
     ##############
     plan
+
+  end
+
+  def make_intermediate_activities
+
+    @results.each do |result|
+
+      previous_plan = nil
+
+      result[:flight_plan].each do |plan|
+
+        if previous_plan.present?
+
+          stay_time = TimeDifference.between(
+              previous_plan[:end_at],
+              plan[:start_at]
+          ).in_hours
+
+          if stay_time > 4
+
+            nights = ( stay_time.to_i / 24 ) > 0 ? ( stay_time.to_i / 24 ) : 1
+
+            previous_plan[:accommodation_plan] = {
+                nights: nights,
+                cost: Airport.find(previous_plan[:arrival_airport_id]).accommodation_cost(nights)
+            }
+
+            previous_plan[:chosen_intermediate_plan] = 'accommodation_plan'
+
+          end
+
+          if stay_time > 24
+
+            aircraft = @aircrafts.find(result[:aircraft_id])
+
+            departure_airport = Airport.find(previous_plan[:arrival_airport_id])
+
+            arrival_airport = aircraft.base_airport
+
+            unless departure_airport == arrival_airport
+
+              previous_plan[:empty_leg_plan] = [
+                  {
+                      departure_airport_id: departure_airport.id,
+                      arrival_airport_id: arrival_airport.id,
+                      flight_type: 'empty_leg',
+                      start_at: previous_plan[:end_at] + 45.minutes,
+                      end_at: previous_plan[:end_at] + 45.minutes + aircraft.flight_time_in_hours_for(departure_airport, arrival_airport).hours,
+                      landing_cost_at_arrival: arrival_airport.landing_cost,
+                      handling_cost_at_takeoff: departure_airport.handling_cost,
+                      flight_cost: aircraft.flight_time_in_hours_for(departure_airport, arrival_airport).hours * aircraft.per_hour_cost
+                  },
+                  {
+                      departure_airport_id: arrival_airport.id,
+                      arrival_airport_id: departure_airport.id,
+                      flight_type: 'empty_leg',
+                      start_at: plan[:start_at] - 45.minutes - aircraft.flight_time_in_hours_for(arrival_airport, departure_airport),
+                      end_at: plan[:start_at] - 45.minutes,
+                      landing_cost_at_arrival: departure_airport.landing_cost,
+                      handling_cost_at_takeoff: arrival_airport.handling_cost,
+                      flight_cost: aircraft.flight_time_in_hours_for(departure_airport, arrival_airport).hours * aircraft.per_hour_cost
+                  }
+              ]
+
+              previous_plan[:chosen_intermediate_plan] = 'empty_leg_plan'
+
+            end
+
+          end
+
+        end
+
+        previous_plan = plan
+
+      end
+
+    end
 
   end
 
