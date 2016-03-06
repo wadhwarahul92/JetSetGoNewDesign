b54346a70659e378aaf465bcf99dace4fb5a206c
MAYUR SINGH <mayur.singh@jetsetgo.in>
2016-03-06 18:02:09 +0530

    templates

--- app/mailers/organisation_mailer.rb
+++ app/mailers/organisation_mailer.rb
@@ -10,7 +10,7 @@ class OrganisationMailer < ApplicationMailer
     @organisation = operator.organisation
     mail(
         to: @operator.email,
-        subject: 'JetSetGo - organisation created'
+        subject: 'JetSetGo - New organisation created'
     )
   end
 
@@ -23,7 +23,7 @@ class OrganisationMailer < ApplicationMailer
     @operator = operator
     mail(
         to: @operator.email,
-        subject: 'JetSetGo - organisation created'
+        subject: 'JetSetGo - New aircraft created'
     )
   end
 
@@ -36,7 +36,7 @@ class OrganisationMailer < ApplicationMailer
     @operator = operator
     mail(
         to: @operator.email,
-        subject: 'JetSetGo - organisation created'
+        subject: 'JetSetGo - Aircraft Deleted'
     )
   end
 
@@ -49,7 +49,7 @@ class OrganisationMailer < ApplicationMailer
     @operator = operator
     mail(
         to: @operator.email,
-        subject: 'JetSetGo - organisation created'
+        subject: 'JetSetGo - Aircraft Edit'
     )
   end
 
@@ -62,7 +62,7 @@ class OrganisationMailer < ApplicationMailer
     @operator = operator
     mail(
         to: @operator.email,
-        subject: 'JetSetGo - organisation created'
+        subject: 'JetSetGo - Aircraft approved by Admin'
     )
   end
 
@@ -74,7 +74,7 @@ class OrganisationMailer < ApplicationMailer
     @operator = operator
     mail(
         to: @operator.email,
-        subject: 'JetSetGo - organisation created'
+        subject: 'JetSetGo - New forum topic created'
     )
   end
 
@@ -86,9 +86,205 @@ class OrganisationMailer < ApplicationMailer
     @operator = operator
     mail(
         to: @operator.email,
-        subject: 'JetSetGo - organisation created'
+        subject: 'JetSetGo - New comment on Forum topic'
     )
   end
 
+  # On add Aircraft unavailability
+  # On edit Aircraft unavailability
+  # On delete Aircraft unavailability
+  #
+  # On add Trip
+  # On edit Trip
+  # On delete Trip
+  #
+  # On Create Enquiry
+  # On Edit Enquiry
+  # On Delete Enquiry
+  #
+  # On Quote
+  # On Edit Quote
+  # On Delete Quote
+  #
+  # On Approved Organisation by Admin
+  # On Approved Operator by Admin
+  # On Set Roles of Operator
+
+  ######################################################################
+  # Description: When a new Aircraft unavailability created
+  ######################################################################
+  def new_aircraft_unavailability(aircraft_unavailability, operator)
+    @aircraft_unavailability = aircraft_unavailability
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - New aircraft unavailability created'
+    )
+  end
+
+  ######################################################################
+  # Description: When Aircraft unavailability Edit
+  ######################################################################
+  def edit_aircraft_unavailability(aircraft_unavailability, operator)
+    @aircraft_unavailability = aircraft_unavailability
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Aircraft unavailability Edit'
+    )
+  end
+
+  ######################################################################
+  # Description: When Aircraft unavailability deleted
+  ######################################################################
+  def delete_aircraft_unavailability(aircraft_unavailability, operator)
+    @aircraft_unavailability = aircraft_unavailability
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Aircraft unavailability deleted'
+    )
+  end
+
+  ######################################################################
+  # Description: When a new trip created
+  ######################################################################
+  def new_trip(trip, operator)
+    @trip = trip
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - New Trip created'
+    )
+  end
+
+  ######################################################################
+  # Description: When trip Edit
+  ######################################################################
+  def edit_trip(trip, operator)
+    @trip = trip
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Edit Trip'
+    )
+  end
+
+  ######################################################################
+  # Description: When trip deleted
+  ######################################################################
+  def delete_trip(trip, operator)
+    @trip = trip
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Trip deleted'
+    )
+  end
+
+  ######################################################################
+  # Description: When a new enquiry created
+  ######################################################################
+  def new_enquiry(enquiry, operator)
+    @enquiry = enquiry
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - New enquiry created'
+    )
+  end
+
+  ######################################################################
+  # Description: When enquiry Edit
+  ######################################################################
+  def edit_enquiry(enquiry, operator)
+    @enquiry = enquiry
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Edit Enquiry'
+    )
+  end
+
+  ######################################################################
+  # Description: When enquiry deleted
+  ######################################################################
+  def delete_enquiry(enquiry, operator)
+    @enquiry = enquiry
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Delete Enquiry'
+    )
+  end
+
+  ######################################################################
+  # Description: When a new quote created
+  ######################################################################
+  def new_quote(quote, operator)
+    @quote = quote
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - New enquiry created'
+    )
+  end
+
+  ######################################################################
+  # Description: When quote Edit
+  ######################################################################
+  def edit_quote(quote, operator)
+    @quote = quote
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Edit quote'
+    )
+  end
+
+  ######################################################################
+  # Description: When quote deleted
+  ######################################################################
+  def delete_quote(quote, operator)
+    @quote = quote
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Delete quote'
+    )
+  end
+
+  ######################################################################
+  # Description: When approved organisation by Super Admin
+  ######################################################################
+  def approved_organisation(operator)
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Approved organisation by Admin'
+    )
+  end
+
+  ######################################################################
+  # Description: When approved operator by Super Admin
+  ######################################################################
+  def approved_operator_by_admin(operator)
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Approve operator by Admin'
+    )
+  end
+
+  ######################################################################
+  # Description: When Admin set roles of an operator
+  ######################################################################
+  def set_roles_of_operator(operator)
+    @operator = operator
+    mail(
+        to: @operator.email,
+        subject: 'JetSetGo - Admin set your role'
+    )
+  end
 
 end
