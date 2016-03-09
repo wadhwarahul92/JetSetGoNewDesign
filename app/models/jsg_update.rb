3fe75ce3d95c7c51159886440df2477829d8bfd2
Suraj Pratap <suraj.pratap@jetsetgo.in>
2016-03-09 14:59:26 +0530

    Update jsg_update.rb

--- app/models/jsg_update.rb
+++ app/models/jsg_update.rb
@@ -4,6 +4,6 @@ class JsgUpdate < ActiveRecord::Base
   validates :description, presence: true
   validates :source_url, presence: true
   validates :image_url, presence: true
-  validates :posted_date, presence: true
+  #validates :posted_date, presence: true
 
 end
