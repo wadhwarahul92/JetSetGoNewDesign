adcd03018de29f6da1cc6eb80ea6f443ac210d7e
Suraj Pratap <surajpratap@icloud.com>
2016-03-15 14:10:52 +0530

    minor change

--- app/controllers/jetsteal_subscriptions_controller.rb
+++ app/controllers/jetsteal_subscriptions_controller.rb
@@ -3,10 +3,29 @@ class JetstealSubscriptionsController < ApplicationController
   protect_from_forgery except: [:create]
 
   def create
-    if params[:from_web].present? and CaptchaValidator.new(params['g-recaptcha-response'], request.remote_ip).validated!
-      @jetsteal_subscription = JetstealSubscription.new(filtered_params)
-      @jetsteal_subscription.send_emails = true
 
+    @jetsteal_subscription = JetstealSubscription.new(filtered_params)
+    @jetsteal_subscription.send_emails = true
+
+    if params[:from_web].present?
+
+      if CaptchaValidator.new(params['g-recaptcha-response'], request.remote_ip).validated!
+        if @jetsteal_subscription.save
+          flash[:success] = 'Thank you for subscribing.'
+          redirect_to (params[:redirect_to] || '/')
+        else
+          flash[:error] = @jetsteal_subscription.errors.full_messages.first
+          redirect_to (params[:redirect_to] || '/')
+        end
+      else
+        flash[:error] = 'Captcha is invalid.'
+        redirect_to(params[:redirect_to] || '/')
+      end
+
+    else
+      ######################################################################
+      # Description: FROM IOS APP
+      ######################################################################
       if @jetsteal_subscription.save
         flash[:success] = 'Thank you for subscribing.'
         redirect_to (params[:redirect_to] || '/')
@@ -14,9 +33,6 @@ class JetstealSubscriptionsController < ApplicationController
         flash[:error] = @jetsteal_subscription.errors.full_messages.first
         redirect_to (params[:redirect_to] || '/')
       end
-    else
-      flash[:error] = 'Captcha is invalid.'
-      redirect_to(params[:redirect_to] || '/')
     end
   end
 
