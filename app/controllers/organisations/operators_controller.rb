265367c705f3db20970e83fd269d79b7cd1a72e3
MAYUR SINGH <mayur.singh@jetsetgo.in>
2016-03-29 13:00:21 +0530

    break

--- app/controllers/organisations/operators_controller.rb
+++ app/controllers/organisations/operators_controller.rb
@@ -9,7 +9,7 @@ class Organisations::OperatorsController < Organisations::BaseController
                                             :forgot_password,
                                             :forgot_password_,
                                             :profile,
-                                            :profile,
+                                            :update_profile,
                                             :toggle,
                                             :set_terms_and_condition,
                                             :get_terms_and_condition
@@ -24,7 +24,7 @@ class Organisations::OperatorsController < Organisations::BaseController
                                               :forgot_password_
   ]
 
-  before_action :authenticate_operator, only: [:index, :edit, :update, :toggle, :profile]
+  before_action :authenticate_operator, only: [:index, :edit, :update, :toggle, :profile, :update_profile]
 
   protect_from_forgery except: [:log_in_]
 
@@ -130,7 +130,11 @@ class Organisations::OperatorsController < Organisations::BaseController
   end
 
   def update_profile
-    1
+    if @operator.update_attributes(operator_params)
+      render status: :ok, nothing: true
+    else
+      render status: :unprocessable_entity, json: { errors: @operator.errors.full_messages }
+    end
   end
 
   def toggle
