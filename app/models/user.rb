9f14b8243709097c1a9e8312e98622f5b33503e1
Suraj Pratap <surajpratap@icloud.com>
2016-05-19 14:49:11 +0530

    profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :phone, presence: true, length: {is: 10}, numericality: true
 
-  has_attached_file :image, presence: false, styles: {small: '50x50!', size_250x250: '250x250!'}
+  has_attached_file :image, presence: false, styles: {small: '50x50!', size_250x250: '250x250!'}, default_url: 'http://d3tfanr7troppj.cloudfront.net/static_files/images/000/000/254/original/image_user.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ , :if => :image_attached?
 
   ###############################
--- app/views/welcome/current_user_.jbuilder
+++ app/views/welcome/current_user_.jbuilder
@@ -3,8 +3,7 @@ if current_user.present?
   json.first_name current_user.first_name
   json.last_name current_user.last_name
   json.full_name current_user.full_name
-  json.image current_user.image.url(:size_250x250) if current_user.image.present?
-
+  json.image current_user.image.url(:size_250x250)
   if current_user.try(:organisation).present?
     json.organisation{
       json.id current_user.organisation.id
