c376af3b53b4d362ddc4fce52dcdbdcce5b55be9
MAYUR SINGH <mayur.singh@jetsetgo.in>
2016-05-19 15:21:14 +0530

    jetsetrescue enquiry done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,7 +31,7 @@
 //= require ./application/jet_set_yatra_controller
 //= require ./application/jet_set_yatra_enquiry_controller
 //= require ./application/jet_set_wed_controller
-//= require ./application/jet_set_wed_controller
+//= require ./application/jet_set_wed_enquiry_controller
 //= require ./application/jet_set_rescue_controller
 //= require ./application/jet_set_rescue_enquiry_controller
 
