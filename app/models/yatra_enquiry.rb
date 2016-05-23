1b2ddfd12c5597bb3eba31cbb7647cced3449440
MAYUR SINGH <mayur.singh@jetsetgo.in>
2016-05-23 16:29:04 +0530

    validation for yatra

--- app/models/yatra_enquiry.rb
+++ app/models/yatra_enquiry.rb
@@ -1,2 +1,9 @@
 class YatraEnquiry < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :mobile_number, presence: true
+  validates :package, presence: true
+  validates :date_of_travel, presence: true
+
 end
