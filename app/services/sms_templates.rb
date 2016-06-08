1088446420c5fb9867b1e1428d9215ec88de9491
rahulwadhwa <rahul.wadhwa@jetsetgo.in>
2016-06-08 14:20:31 +0530

    update

--- app/services/sms_templates.rb
+++ app/services/sms_templates.rb
@@ -6,6 +6,14 @@ class SmsTemplates
       "Thank you for your payment towards #{service}. For any queries please feel free to call 01139585858."
     end
 
+    def customer_create_enquiry(service)
+      "Thank you for your enquiry towards #{service}. For any queries please feel free to call 01139585858."
+    end
+
+    def generate_quote
+      "Quote has been generated. Please check your account. For any queries please feel free to call 01139585858."
+    end
+
   end
 
 end
