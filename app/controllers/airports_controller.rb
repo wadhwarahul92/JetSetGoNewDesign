ca339c880322df54490331aa042e4cece80b3477
Suraj Pratap <surajpratap@icloud.com>
2016-07-01 11:32:30 +0530

    airports name asc order

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,11 +1,11 @@
 class AirportsController < ApplicationController
 
   def index
-    @airports = Airport.includes(:city).all
+    @airports = Airport.order('name ASC').includes(:city).all
   end
 
   def all_names
-    @airport_names = Airport.all.map(&:name)
+    @airport_names = Airport.order('name ASC').all.map(&:name)
     render json: @airport_names.to_json
   end
 
