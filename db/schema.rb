7fab99e6a7e319a7cf798012f93becdec3400062
Suraj Pratap <surajpratap@icloud.com>
2016-07-04 12:06:19 +0530

    deploying

--- db/schema.rb
+++ db/schema.rb
@@ -145,8 +145,8 @@ ActiveRecord::Schema.define(version: 20160702051520) do
   create_table "airports", force: :cascade do |t|
     t.string   "name",                        limit: 255
     t.integer  "city_id",                     limit: 4
-    t.datetime "created_at",                                              null: false
-    t.datetime "updated_at",                                              null: false
+    t.datetime "created_at",                                            null: false
+    t.datetime "updated_at",                                            null: false
     t.float    "longitude",                   limit: 24
     t.float    "latitude",                    limit: 24
     t.string   "code",                        limit: 255
@@ -162,7 +162,7 @@ ActiveRecord::Schema.define(version: 20160702051520) do
     t.float    "landing_cost",                limit: 24,  default: 0.0
     t.datetime "deleted_at"
     t.integer  "bais_time_in_minutes",        limit: 4,   default: 0
-    t.boolean  "atc",                                     default: false
+    t.boolean  "atc"
     t.integer  "airport_category_id",         limit: 4
     t.float    "landing_minimum_mtow",        limit: 24,  default: 0.0
     t.float    "landing_maximum_mtow",        limit: 24,  default: 0.0
@@ -296,6 +296,7 @@ ActiveRecord::Schema.define(version: 20160702051520) do
     t.text     "description", limit: 65535
     t.text     "source_url",  limit: 65535
     t.text     "image_url",   limit: 65535
+    t.date     "posted_date"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.datetime "deleted_at"
@@ -483,13 +484,6 @@ ActiveRecord::Schema.define(version: 20160702051520) do
     t.datetime "updated_at",                    null: false
   end
 
-  create_table "terms_and_conditions", force: :cascade do |t|
-    t.text     "description",     limit: 65535
-    t.integer  "organisation_id", limit: 4
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-  end
-
   create_table "testimonials", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.text     "text",       limit: 65535
@@ -538,6 +532,10 @@ ActiveRecord::Schema.define(version: 20160702051520) do
     t.string   "ios_app_devise_token",     limit: 255
     t.string   "android_app_devise_token", limit: 255
     t.boolean  "send_app_notifications",                 default: true
+    t.string   "avatar_file_name",         limit: 255
+    t.string   "avatar_content_type",      limit: 255
+    t.integer  "avatar_file_size",         limit: 4
+    t.datetime "avatar_updated_at"
     t.string   "image_file_name",          limit: 255
     t.string   "image_content_type",       limit: 255
     t.integer  "image_file_size",          limit: 4
