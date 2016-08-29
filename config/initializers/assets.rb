569699e6f2e608713d4b31a390fd6b151cfb3c37
Rahul Wadhwa <rahul.wadhwa@jetsetgo.in>
2016-08-29 18:52:37 +0530

    asset file change

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(admin.css admin.js jetsteals.css jetsteals.js operators.css operators.js organisations.css organisations.js vendors/bg-slider/bar.js vendors/bg-slider/vegas.min.js vendors/bg-slider/slider_vegas_app.min.js)
+Rails.application.config.assets.precompile += %w( vendors/bg-slider/bar.js )
