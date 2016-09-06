bfa2d981a7c5c43ac79557678509a7f044b17b32
Mayur <mayursingh1220@gmail.com>
2016-09-06 15:23:41 +0530

    minor

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -1,7 +1,7 @@
 class AircraftsController < ApplicationController
 
   def index
-    @aircrafts = Aircraft.where(id: params[:ids]).includes(:aircraft_images, :aircraft_type, :base_airport)
+    @aircrafts = Aircraft.where(id: params[:ids]).includes(:aircraft_images, :aircraft_type, :base_airport )
   end
 
   def all_aircrafts
