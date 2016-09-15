46fdb03f4c6c69ffb6d33bcdf5c3d457c6a67bb8
Mayur <mayursingh1220@gmail.com>
2016-09-15 15:36:39 +0530

    update

--- app/services/sms_templates.rb
+++ app/services/sms_templates.rb
@@ -2,14 +2,6 @@ class SmsTemplates
 
   class<<self
 
-    def customer_sign_up(service, web_link)
-      "Welcome to #{service}, #{web_link} is your boarding pass to the new face of private aviation."
-    end
-
-    def operator_sign_up(service)
-      "Thank you for signing up with #{service} – your ticket to a whole new world of opportunities."
-    end
-
     def thanks_for_payment(service)
       "Thank you for your payment towards #{service}. For any queries please feel free to call 01139585858."
     end
@@ -18,16 +10,24 @@ class SmsTemplates
       "Thank you for your enquiry towards #{service}. For any queries please feel free to call 01139585858."
     end
 
+    def generate_quote
+      "Quote has been generated. Please check your account. For any queries please feel free to call 01139585858."
+    end
+
+    def customer_sign_up(service, web_link)
+      "Welcome to #{service}, #{web_link} is your boarding pass to the new face of private aviation."
+    end
+
+    def operator_sign_up(service)
+      "Thank you for signing up with #{service} – your ticket to a whole new world of opportunities."
+    end
+
     def customer_for_enquiry(service)
       "#{service} will process your enquiry very shortly."
     end
 
     def operator_for_enquiry(link)
       "You have an enquiry – your customer is waiting. Click here for further details #{link}"
-    end
-
-    def generate_quote
-      "Quote has been generated. Please check your account. For any queries please feel free to call 01139585858."
     end
 
     def customer_for_quote(link)
