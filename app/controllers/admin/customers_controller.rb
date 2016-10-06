6f7c812c04e577ebf1f371e7cff2dfdeceb8e48d
Mayur <mayursingh1220@gmail.com>
2016-10-06 14:33:59 +0530

    update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,7 +3,7 @@ class Admin::CustomersController < Admin::BaseController
   before_filter :authenticate_admin
 
   def index
-    @customers = User.where("type is null").paginate(page: params[:page], per_page: 25)
+    @customers = User.where("type is null").order(id: :desc).paginate(page: params[:page], per_page: 25)
   end
 
 end
