1d7023f429d77cab58bd7b0789e356c8267ad9bc
Mayur <mayursingh1220@gmail.com>
2016-10-06 13:04:08 +0530

    update

--- app/controllers/admin/enquiries_controller.rb
+++ app/controllers/admin/enquiries_controller.rb
@@ -5,7 +5,7 @@ class Admin::EnquiriesController < Admin::BaseController
   before_action :set_enquiry, only: [:show]
 
   def index
-    @enquiries = Trip.where(status: Trip::STATUS_ENQUIRY).order(id: :desc)
+    @enquiries = Trip.where(status: Trip::STATUS_ENQUIRY).order(id: :desc).all.paginate(page: params[:page], per_page: 50)
   end
 
   def show
